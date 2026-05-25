c7ada02a6a852be4e9b2ee981eebb769db0c79e1
Anurag Kumar Kanojiya <49854242+ak909412@users.noreply.github.com>
2026-05-25 18:07:41 +0530

    Update server.js

--- server.js
+++ server.js
@@ -18,6 +18,8 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors());
+app.use(express.json({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb' }));
 
